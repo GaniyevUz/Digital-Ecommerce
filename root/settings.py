7b89ab1c919c802952ae4fd771105f088d3d94c7
jakhongir <ganiyevuzb@gmail.com>
2023-01-24 21:45:46 +0500

    registered local apps and migraded to db

--- root/settings.py
+++ root/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # installed apps
+    # My apps
+    'users.apps.UsersConfig',
+    'shops.apps.ShopsConfig',
+    'orders.apps.OrdersConfig',
+    'telegrambots.apps.TelegrambotsConfig',
+
+    # Third-party apps
     'mptt',
     'rest_framework',
     'drf_yasg',
@@ -76,6 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'root.wsgi.application'
 
+AUTH_USER_MODEL = 'users.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -86,7 +91,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -106,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -118,7 +121,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -128,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
--- users/models.py
+++ users/models.py
@@ -5,7 +5,8 @@ from mptt.models import MPTTModel
 
 
 # Create your models here.
-class User(AbstractUser, MPTTModel):
-    default_shop = models.ForeignKey('shops.Shop', on_delete=models.CASCADE, null=True, blank=True)
+class User(AbstractUser):
+    default_shop = models.OneToOneField('shops.Shop', models.CASCADE, related_name='default_shop', null=True, blank=True)
     invitation_token = models.CharField(max_length=255, null=True)
-    invitation = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
+    invitation = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True, blank=True)
+    # invitation = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
