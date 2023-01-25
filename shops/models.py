0b8362ae72127c9aa5fe1b8a56457ae44d50df5b
rashideveloper <bubbless7456@gmail.com>
2023-01-25 17:41:26 +0500

    created simple category object

--- shops/models.py
+++ shops/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
+class ShopCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+
 # Create your models here.
 class Shop(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', models.CASCADE)
+    shop_category = models.ForeignKey('shops.ShopCategory', models.CASCADE)
