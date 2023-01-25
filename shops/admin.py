c6a944d827f80d3ef4b0357c720c071d2d59d17e
rashideveloper <bubbless7456@gmail.com>
2023-01-25 21:04:20 +0500

    aded Jazzmin Admin interface

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from shops.models import Category
+
 
 # Register your models here.
+
+@admin.register(Category)
+class ShopCategory(ModelAdmin):
+    list_display = ('pk', 'name')
--- shops/models.py
+++ shops/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 
-class ShopCategory(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Shop Category'
+        verbose_name_plural = 'Shop Categories'
 
 
 # Create your models here.
 class Shop(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey('users.User', models.CASCADE)
-    shop_category = models.ForeignKey('shops.ShopCategory', models.CASCADE)
+    shop_category = models.ForeignKey('shops.Category', models.CASCADE)
