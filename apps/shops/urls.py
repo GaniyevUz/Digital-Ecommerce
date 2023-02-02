ffffc3b3a86dfe6f0514c4d7a57d33dfb5cba897
rashideveloper <bubbless7456@gmail.com>
2023-02-02 14:40:19 +0500

    order

--- apps/shops/urls.py
+++ apps/shops/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from shops.views import ShopListCreateAPIView, ShopRetrieveUpdateDestroyAPIView, CategoryModelViewSet, CurrencyModelViewSet, PaymentProvidersViewSet
+from shops.views import ShopListCreateAPIView, ShopRetrieveUpdateDestroyAPIView, CategoryModelViewSet, \
+    CurrencyModelViewSet, PaymentProvidersViewSet, ShopOrdersRetrieveAPIView
 
 router = DefaultRouter()
 router.register('category', CategoryModelViewSet, 'category')
@@ -11,5 +12,6 @@ router.register('payment', PaymentProvidersViewSet, 'payment')
 urlpatterns = [
     path('shop', ShopListCreateAPIView.as_view(), name='shop'),
     path('shop/<int:pk>/detail', ShopRetrieveUpdateDestroyAPIView.as_view(), name='detail'),
+    # path('shop/<int:pk>/order', ShopOrdersRetrieveAPIView.as_view(), name='orders'),
     path('', include(router.urls)),
 ]
