40dfa9de51736878e4e58fd8075859d39b5344cd
rashideveloper <bubbless7456@gmail.com>
2023-02-02 19:40:56 +0500

    shop test fixed

--- apps/shops/views/shop.py
+++ apps/shops/views/shop.py
@@ -17,14 +17,14 @@ class ShopModelViewSet(ModelViewSet, CountResultMixin):
     def get_queryset(self):
         return self.request.user.shop_set.all()
 
+    def list(self, request, *args, **kwargs):
+        return self.count_result_list(request, *args, **kwargs)
+
     @action(methods=['GET'], detail=True, url_name='category', url_path='category')
     def get_category(self, request, pk):
         shop = get_object_or_404(Shop, pk=pk)
         serializer_data = self.serializer_class(shop.category_set.all(), many=True)
         return Response(serializer_data.data)
-
-    def list(self, request, *args, **kwargs):
-        return self.count_result_list(request, *args, **kwargs)
 
 
 class PaymentProvidersListAPIView(ListAPIView):
