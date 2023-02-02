e7669619702f218cb41744e3a22d186ce2123990
rashideveloper <bubbless7456@gmail.com>
2023-02-02 18:15:25 +0500

    nimadur

--- apps/shops/views/shop.py
+++ apps/shops/views/shop.py
@@ -1,32 +1,28 @@
-from rest_framework import status
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, ListAPIView
+from rest_framework.decorators import action
+from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from shops.models import Shop
 from shops.models.shop_belongs import PaymentProviders
 from shops.serializers import ShopSerializer, PaymentSerializers
 
 
-class ShopListCreateAPIView(ListCreateAPIView):
+class ShopModelViewSet(ModelViewSet):
     serializer_class = ShopSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return self.request.user.shop_set.all()
 
-    def get(self, request, *args, **kwargs):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        data = {
-            'count': self.get_queryset().count(),
-            'result': serializer.data
-        }
-        return Response(data, status.HTTP_200_OK)
+    @action(methods=['GET'], detail=True, url_name='category', url_path='category')
+    def get_category(self, request, pk):
+        shop = get_object_or_404(Shop, pk=pk)
+        serializer_data = self.serializer_class(shop.category_set.all(), many=True)
+        return Response(serializer_data.data)
 
 
-class ShopRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Shop.objects.all()
-    serializer_class = ShopSerializer
-
-
-class Payment_providersListAPIView(ListAPIView):
+class PaymentProvidersListAPIView(ListAPIView):
     queryset = PaymentProviders.objects.all()
     serializer_class = PaymentSerializers
