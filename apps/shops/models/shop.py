e3a7f5d2f4f56a2eeccd321f7d87960ab27401e2
rashideveloper <bubbless7456@gmail.com>
2023-02-10 03:16:09 +0500

    fake

--- apps/shops/models/shop.py
+++ apps/shops/models/shop.py
@@ -13,11 +13,11 @@ class Shop(models.Model):
         DELIVERY = 'delivery', 'Delivery'
 
     name = models.CharField(max_length=255)
-    languages = MultiSelectField(max_length=15, choices=Languages.choices, min_choices=1)
+    languages = MultiSelectField(max_length=255, choices=Languages.choices, min_choices=1)
     user = models.ForeignKey('users.User', models.CASCADE)
     shop_currency = models.ForeignKey('shops.Currency', models.RESTRICT)
     shop_category = models.ForeignKey('shops.Category', models.RESTRICT)
-    delivery_types = MultiSelectField(max_length=15, choices=Delivery.choices, min_choices=1, default=Delivery.PICKUP)
+    delivery_types = MultiSelectField(max_length=255, choices=Delivery.choices, min_choices=1, default=Delivery.PICKUP)
     about_us = models.CharField(max_length=1024, null=True, blank=True)
     delivery_price = models.IntegerField('Delivery Price', null=True, blank=True)
     delivery_price_per_km = models.IntegerField('Delivery Price Per KM', null=True, blank=True)
