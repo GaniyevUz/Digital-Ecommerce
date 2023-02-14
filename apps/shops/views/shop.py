b772a6ddec6f5b64f168c015a16b85042a8bc9e3
muhammad <abduvaliyevmuhammad41@gmail.com>
2023-02-14 11:15:49 +0500

    all/stat

--- apps/shops/views/shop.py
+++ apps/shops/views/shop.py
@@ -1,9 +1,12 @@
+from django.db.models import Count
+from django.db.models.expressions import RawSQL
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from orders.models import Order
 from orders.serializers import OrderModelSerializer
 from products.serializers import CategoryModelSerializer
 from shared.mixins import CountResultMixin
@@ -47,6 +50,29 @@ class ShopModelViewSet(ModelViewSet, CountResultMixin):
     def shop_config(self, request):
         langs = (("🇺🇿", "O'zbekcha", "uz"), ("🇷🇺", "Русский", "ru"), ("🇺🇸", "English", "en"))
         data = {"languages": [{'icon': i, 'title': t, 'code': c} for i, t, c in langs]}
+        return Response(data)
+
+    @action(['GET'], True, 'stat/all', 'stat_all')
+    def main_stat(self, request, pk=None):
+
+        orders = Order.objects.filter(items__order__shop_id=pk).annotate(total_items=Count('items'))
+        total_orders = sum(orders.values_list('total_items', flat=True))
+
+        paid_orders = orders.filter(paid=True).annotate(total_items=Count('items'))
+        paid_orders_cost = sum(paid_orders.values_list('total_items', flat=True))
+
+        _ = Order.objects.values('id').annotate(summ=RawSQL("SELECT get_summ_all(%s)", (1,)),
+                                                avg=RawSQL("SELECT get_avarage_price(%s)", (1,)))[0]
+        revenue, avg = _['summ'], _['avg']
+        # total_customers = Client.objects.filter(shop_id=pk).count() # client chala
+        data = {
+            'id': pk,
+            'total_orders': total_orders,
+            'paid_orders': paid_orders_cost,
+            'total_revenue': revenue,
+            'total_customers': 'chala',
+            'average_price': avg
+        }
         return Response(data)
 
 
