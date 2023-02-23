8d2ee2282d2b43718bb912ee24e8748da2598a71
xolmomin <xolmomin@gmail.com>
2023-02-23 16:54:01 +0500

    change url

--- apps/urls.py
+++ apps/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('', include(('users.urls', 'users'), 'users')),
     path('shop/', include(('shops.urls', 'shops'), 'shops')),
     path('', include(('products.urls', 'products'), 'products')),
-    path('ecommerce/<int:shop>/', include('ecommerce.urls'))
+    path('ecommerce/', include('ecommerce.urls'))
 ]
