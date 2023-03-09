9a06a8be554ceaaeb1af60a9086a7dc99ff31995
jakhongir <ganiyevuzb@gmail.com>
2023-03-09 14:39:29 +0500

    fixed bugs

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,8 +1,6 @@
 from django_filters import rest_framework as filters
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from rest_framework.response import Response
 
 from shared.django import APIViewSet
